720302c434e13a40a65394743b68351e1a756315
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-02-22 23:25:23 +0000

    exercice1

---
+++ exercices/exercice1.py
@@ -0,0 +1,18 @@
+# Creation d'une liste de 5 nombres.
+number = [1 ,2 ,3 ,4 ,1 ,0 ,8 ,7 ,4]
+
+# Ajout d'un element à la fin
+number.append(12)
+
+# Ajout d'un element au debut
+
+number.insert(0, 5)
+print(number)
+
+# Suppression du troisième element
+number.remove(2)
+print(number)
+
+# Trier le tableau de façon croissant
+number.sort()
+print(number)
