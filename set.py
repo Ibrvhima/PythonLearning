71a2c6a43b82f6df577f9285c0b697cb2772b6e3
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-02-22 23:06:18 +0000

    structure des donnees (les set)

---
+++ set.py
@@ -0,0 +1,14 @@
+# Les ensembles en python
+
+sets = {1,2,3,4,5,6,6,8,8,9,7,0}
+
+print(sets)
+
+# Ajout d'un element à l'ensemble
+
+sets.add(10)
+print(sets)
+
+# Supprimer d'un element
+sets.remove(0)
+print(sets)
