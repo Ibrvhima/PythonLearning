16c0d1afc987a055717734e1cbad2b0ce625ebb0
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-02-23 00:53:31 +0000

    exercices 5

---
+++ exercices/exercice5.py
@@ -0,0 +1,15 @@
+# Creation d'un tuple
+t = (1, 2, 3, 4, 5)
+
+# Conversion d'un tuple en u list
+tc = list(t)
+print(tc)
+
+# Ajout d'un element avec la liste 
+tc.append(6)
+print(tc)
+
+# Reconversion de la list
+tp = tuple(tc)
+print(tp)
+
