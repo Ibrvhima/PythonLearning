80c2a4255336f78e7d77286b41d595197a73880c
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-02-23 00:10:29 +0000

    exercice2

---
+++ exercices/exercice2.py
@@ -0,0 +1,8 @@
+# Creation d'un tableau nombre
+my_list = [10, 23, 45, 60, 75, 90, 105]
+
+# Affichages des nombres paire
+for np in my_list:
+     if(np % 2 == 0):
+           print(np)    
+           
